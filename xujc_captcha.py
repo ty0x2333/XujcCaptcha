e9ab7e8c98fe4c48e7fe2f9c1dd54eab6521b9d5
luckytianyiyan <luckytianyiyan@gmail.com>
2016-05-02 13:34:53 +0800

    M add XujcCaptcha

--- xujc_captcha.py
+++ xujc_captcha.py
@@ -9,74 +9,99 @@ __version__ = "1.0"
 DEFAULT_THRESHOLD = 200
 
 
-def check(xy, img, matrix):
-    try:
-        return img.getpixel(xy) < 255 and matrix[xy[0]][xy[1]] != -1
-    except:
-        return False
+class XujcCaptcha:
 
+    def __init__(self, threshold=DEFAULT_THRESHOLD):
+        self.threshold = threshold
 
-def juli(r,s):
-    return abs(r[0]-s[0])+abs(r[1]-s[1])+abs(r[2]-s[2])
+    def recognition(self, image):
+        # convert to Gray Scale Image
+        gray_img = image.convert('L')
+        # gray_img.save(r'./gray.png')
 
+        self.__clear_noise(gray_img)
 
-def clear_noise(img):
-    width = img.size[0]
-    height = img.size[1]
-    matrix = [-1] * width
-    for i in range(len(matrix)):
-        matrix[i] = [-1] * height
+        # convert to Binary Image
+        table = []
+        for i in range(256):
+            if i < self.threshold:
+                table.append(0)
+            else:
+                table.append(1)
 
-    maps = {}
-    n = 0
+        gray_img = gray_img.point(table, '1')
+        # gray_img.save(r'./clear_noise.png')
 
-    for y in xrange(0, height):
-        for x in xrange(0, width):
-            center = (x, y)
-            left = (x - 1, y)
-            right = (x + 1, y)
-            top = (x, y - 1)
-            bottom = (x, y + 1)
-            left_bottom = (x - 1, y + 1)
-            right_bottom = (x + 1, y + 1)
-            left_top = (x - 1, y - 1)
-            right_top = (x + 1, y - 1)
-            r = [0, 0, 0]
-            s = [0, 0, 0]
-            if img.getpixel(center) < 255:
-                if check(left, img, matrix):# left
-                    matrix[x][y] = matrix[x - 1][y]
-                    maps[str(matrix[x][y])] += 1
-                elif check(left_top, img, matrix):# left-top
-                    matrix[x][y] = matrix[x - 1][y - 1]
-                    maps[str(matrix[x][y])] += 1
-                elif check(top, img, matrix):# top
-                    matrix[x][y] = matrix[x][y - 1]
-                    maps[str(matrix[x][y])] += 1
-                elif check(right_bottom, img, matrix):# right-bottom
-                    matrix[x][y] = matrix[x + 1][y + 1]
-                    maps[str(matrix[x][y])] += 1
-                elif check(bottom, img, matrix):# bottom
-                    matrix[x][y] = matrix[x][y + 1]
-                    maps[str(matrix[x][y])] += 1
-                elif check(left_bottom, img, matrix):# left-bottom
-                    matrix[x][y] = matrix[x-1][y + 1]
-                    maps[str(matrix[x][y])] += 1
-                elif check(right_top, img, matrix):# right-top
-                    matrix[x][y] = matrix[x+1][y - 1]
-                    maps[str(matrix[x][y])] += 1
-                elif check(right, img, matrix):# right
-                    matrix[x][y] = matrix[x + 1][y]
-                    maps[str(matrix[x][y])] += 1
-                else:
-                    n += 1
-                    maps[str(n)]=1
-                    matrix[x][y] = n
+        return image_to_string(gray_img, lang='xujc')
 
-    for x in range(width):
-        for y in range(height):
-            if matrix[x][y] != -1 and maps[str(matrix[x][y])] <= 2:
-                img.putpixel((x, y), 255)
+    @staticmethod
+    def __check(xy, img, matrix):
+        try:
+            return img.getpixel(xy) < 255 and matrix[xy[0]][xy[1]] != -1
+        except:
+            return False
+
+    @staticmethod
+    def __juli(r, s):
+        return abs(r[0]-s[0])+abs(r[1]-s[1])+abs(r[2]-s[2])
+
+    def __clear_noise(self, img):
+        width = img.size[0]
+        height = img.size[1]
+        matrix = [-1] * width
+        for i in range(len(matrix)):
+            matrix[i] = [-1] * height
+
+        maps = {}
+        n = 0
+
+        for y in xrange(0, height):
+            for x in xrange(0, width):
+                center = (x, y)
+                left = (x - 1, y)
+                right = (x + 1, y)
+                top = (x, y - 1)
+                bottom = (x, y + 1)
+                left_bottom = (x - 1, y + 1)
+                right_bottom = (x + 1, y + 1)
+                left_top = (x - 1, y - 1)
+                right_top = (x + 1, y - 1)
+                r = [0, 0, 0]
+                s = [0, 0, 0]
+                if img.getpixel(center) < 255:
+                    if self.__check(left, img, matrix):# left
+                        matrix[x][y] = matrix[x - 1][y]
+                        maps[str(matrix[x][y])] += 1
+                    elif self.__check(left_top, img, matrix):# left-top
+                        matrix[x][y] = matrix[x - 1][y - 1]
+                        maps[str(matrix[x][y])] += 1
+                    elif self.__check(top, img, matrix):# top
+                        matrix[x][y] = matrix[x][y - 1]
+                        maps[str(matrix[x][y])] += 1
+                    elif self.__check(right_bottom, img, matrix):# right-bottom
+                        matrix[x][y] = matrix[x + 1][y + 1]
+                        maps[str(matrix[x][y])] += 1
+                    elif self.__check(bottom, img, matrix):# bottom
+                        matrix[x][y] = matrix[x][y + 1]
+                        maps[str(matrix[x][y])] += 1
+                    elif self.__check(left_bottom, img, matrix):# left-bottom
+                        matrix[x][y] = matrix[x-1][y + 1]
+                        maps[str(matrix[x][y])] += 1
+                    elif self.__check(right_top, img, matrix):# right-top
+                        matrix[x][y] = matrix[x+1][y - 1]
+                        maps[str(matrix[x][y])] += 1
+                    elif self.__check(right, img, matrix):# right
+                        matrix[x][y] = matrix[x + 1][y]
+                        maps[str(matrix[x][y])] += 1
+                    else:
+                        n += 1
+                        maps[str(n)]=1
+                        matrix[x][y] = n
+
+        for x in range(width):
+            for y in range(height):
+                if matrix[x][y] != -1 and maps[str(matrix[x][y])] <= 2:
+                    img.putpixel((x, y), 255)
 
 
 def main():
@@ -104,26 +129,10 @@ def main():
 
     image = Image.open(filename)
     logging.info('image size: %s' % str(image.size))
-    # convert to Gray Scale Image
-    gray_img = image.convert('L')
-    gray_img.save(r'./gray.png')
+    logging.info('threshold: %d' % options.threshold)
 
-    clear_noise(gray_img)
-
-    # convert to Binary Image
-    threshold = options.threshold
-    logging.info('threshold: %d' % threshold)
-    table = []
-    for i in range(256):
-        if i < threshold:
-            table.append(0)
-        else:
-            table.append(1)
-
-    gray_img = gray_img.point(table, '1')
-    gray_img.save(r'./clear_noise.png')
-
-    print image_to_string(gray_img, lang='xujc')
+    captcha = XujcCaptcha(options.threshold)
+    print captcha.recognition(image)
 
 
 if __name__ == '__main__':
